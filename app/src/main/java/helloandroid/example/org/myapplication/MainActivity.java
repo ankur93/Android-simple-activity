8b4b65abaf26346381e7495762c60b07939b67b7
Ankur Agrawal <agrawalankur93@outlook.com>
2015-05-21 00:37:52 +0530

    third commit

--- app/src/main/java/helloandroid/example/org/myapplication/MainActivity.java
+++ app/src/main/java/helloandroid/example/org/myapplication/MainActivity.java
@@ -1,17 +1,29 @@
 package helloandroid.example.org.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainactivity);
+
+        Button b = (Button) findViewById(R.id.my_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+
+            }
+        });
     }
 
     @Override
