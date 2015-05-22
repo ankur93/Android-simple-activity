bad6e4ef7c40438817bcb0ec36ced09fa790c63f
aagrawal13 <aagrawal13@sapient.com>
2015-05-22 22:14:47 +0530

    Fourth Commit

--- app/src/main/java/helloandroid/example/org/myapplication/CallReceiver.java
+++ app/src/main/java/helloandroid/example/org/myapplication/CallReceiver.java
@@ -3,6 +3,9 @@ package helloandroid.example.org.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 /**
  * Created by aagr12 on 5/22/2015.
@@ -10,6 +13,15 @@ import android.content.Intent;
 public class CallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        if(extras!=null){
+            String phoneState = extras.getString(TelephonyManager.EXTRA_STATE);
+            Log.d("Call",phoneState);
 
+            if(phoneState.equals(extras.getString(TelephonyManager.EXTRA_STATE_RINGING))){
+                String phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.d("Call",phoneNumber);
+            }
+        }
     }
 }
